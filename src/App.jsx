8a8998b57b7800f872ea1e965224f6a69347f031
Eliana Ughetto <elianaughetto10@gmail.com>
2023-11-24 18:36:11 -0300

    fixed the navigation routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GetSign from "./pages/GetSign";
-import NavBar from "./components/NavBarComponent";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import PlanetInfo from "./pages/PlanetInfo";
+import UserHome from "./pages/UserHome";
+
 
 function App() {
   return (
     <div>
-      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/get-sign" element={<GetSign />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/user-home" element={<UserHome />} />
+        <Route path="/planet-info/:planetName" element={<PlanetInfo />} />
       </Routes>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 // main.tsx or main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
